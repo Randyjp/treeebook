bc73baa24af4cdeb403c2ed12a9eabd5ab3d7c0e
Randy Perez <randyjp125@gmail.com>
2013-11-05 00:26:14 -0400

    Updated fronted end for the new authentication system
    added simple_form
    Updated several forms to boostrap and simple_form
    Addded loging, logout, register and profiling to our nav
    Adding a collection select to out status form

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,4 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :name
+  attr_accessible :content, :user_id, :integer # aqui se dice los valores que se pueden enviar en un form 
+  belongs_to :user  # dice que un usuario tiene muchos status
 end
